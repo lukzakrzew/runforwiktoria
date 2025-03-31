92c3572157547c1063ec6f392e4681309f27e123
Lukasz Zakrzewski <lukzakrzew@gmail.com>
2025-03-31 09:10:52 +0200

    translations and new logo

---
+++ assets/js/eng.js
@@ -0,0 +1,68 @@
+const eng = {
+  header: "Run for Wiktoria",
+  subheader1: "Run Beyond Limits",
+  subheader2: "Help Wiktoria Take Her First Step!",
+  button1: "Her story",
+  button2: "How to help",
+  "sp1-Header": "✨ Meet Wiktoria – A Little Warrior",
+  "sp1-text1": "Before she was even born, Wiktoria had to fight for her life.",
+  "sp1-text2":
+    "Weighing just 700 grams, she underwent her first prenatal surgery to close a spinal defect caused by spina bifida",
+  "sp1-text3":
+    "Her early months were filled with challenges—multiple surgeries, intensive hospital stays, and life-threatening complications. ⚕️ Now, at almost three years old, Wiktoria continues to battle the effects of her condition such as:",
+  "sp1-list1": "Spinal cord damage",
+  "sp1-list2": "Muscle paralysis",
+  "sp1-list3": "Hydrocephalus",
+  "sp1-list4": "Neurogenic bladder",
+  "sp1-list5": "Complex vision impairment (strabismus, nystagmus)",
+  "sp1-list6": "Pulmonary stenosis",
+  "sp2-header": "Behind Her Smile – A Battle Fought Every Day",
+  "sp2-text1":
+    "Wiktoria’s laughter and bright spirit hide the immense effort she puts in every single day. While she appears just like any other child, her journey is <strong>filled with tireless work</strong>, dedication, and the unwavering support of therapists, doctors, and psychologists who fight alongside her.",
+  "sp2-text2":
+    "She undergoes 👐 <strong>intensive therapy sessions every day</strong>, working relentlessly to strengthen her body and gain as much independence as possible. Her rehabilitation includes the Vojta Method, deep tissue massage, and NDT therapy—all essential in stimulating her nervous system and preventing complications.",
+  "sp2-text3":
+    "Despite her incredible determination, <strong>Wiktoria faces a great unknown.</strong> Due to flaccid paralysis in her legs, no one can say if she will ever be able to walk. But that does not stop her. Every day, she gives her all, proving that strength is not measured in steps taken, but in the <strong>courage to keep moving forward.</strong>",
+  "sp3-header": "Wiktoria is a joyful and bright little girl",
+  "sp3-text1":
+    "Just like other children her age, she loves exploring the world around her. She enjoys 📚reading books, 🎤sings songs beautifully, and 🗣️speaks very clearly.",
+  "sp3-text2":
+    "🐶 She adores <b>animals, especially dogs and cats</b>. She also loves spending time at daycare, where she eagerly interacts with other children, caregivers, and therapists.",
+  "sp3-text3":
+    "Although she faces certain health challenges, they do not define her. Wiktoria is a happy, loving child who <strong>embraces life with a big smile</strong> 👧, proving every day that she is just like any other little girl — <strong>full of dreams, joy, and determination.</strong>",
+  "sp4-header": "Help Wiktoria Take Her First Steps!",
+  "sp4-text1":
+    "Wiktoria’s journey 🚀 toward independence is a path filled with challenges, but with your support 🤝, she can take one step closer to a brighter future.",
+  "sp4-text2": "💰 The funds raised will go toward covering costs of:",
+  "sp4-list1":
+    "Essential medical equipment such as an active wheelchairs, orthoses",
+  "sp4-list2": "Ongoing therapy",
+  "sp4-list3": "Doctor visits, and specialist consultations",
+  "sp4-text3":
+    "Your generosity 🙏 will help cover these life-changing costs, making it possible for Wiktoria to continue her rehabilitation and one day walk on her own. Please, help Wiktoria take her first steps. Every donation counts, and together we can make a world of difference in her life.",
+  "help-header": "🤝 How You Can Help",
+  "help-section1-header": "Make a Donation",
+  "help-section1-text":
+    "Every contribution, big or small, brings Wiktoria closer to a more independent future. There are many ways you can support her journey! 💖",
+  "help-section1-button": "Join our supporters",
+  "help-section2-header": "Donate 1.5% of Your Tax (Poland)",
+  "help-section2-text1":
+    "Polish residents can support Wiktoria at no extra cost by donating 1.5% of their annual tax.",
+  "help-section2-text2": "Simply enter:",
+  "help-section4-text3": "KRS: 0000387207",
+  "help-section4-text4": 'Purpose: "3118 Wiktoria Sawicka"',
+  "help-section3-header": "Run for Wiktoria",
+  "help-section3-text1":
+    "Are you participating in a marathon or any other race? Dedicate your run to Wiktoria! By running for her, you help spread awareness and inspire others to support her journey.",
+  "help-section3-text2":
+    "💬 Let us know! Tag us on Instagram: <b>@runforwiktoria</b> and use the hashtags:",
+  "help-section3-text3":
+    "Every step you take, every post you share, helps make a difference. Together, we can reach more hearts and bring Wiktoria closer to her dream of walking!",
+  "help-section4-header": "Spread the Word",
+  "help-section4-text1":
+    "Share Wiktoria’s story with your friends and family. The more people who know about her journey, the more support she can receive.",
+  "help-section5-header": "Corporate Sponsorship",
+  "help-section5-text1":
+    'Companies and organizations can support Wiktoria by becoming an Honorary Patron of the "Run for Wiktoria" initiative. <br><br> Sponsors will be featured on our website and social medias as a sign of gratitude for their generosity. <br><br> If your company is interested in becoming sponsor, please contact us.',
+  "help-section5-text2": "📩 Get in touch",
+};
